de622ac1b8aa7bb613d9cec96c841d29624959bd
rony klein <ronyklein@adams-MacBook-Pro-2.local>
2015-08-05 15:15:21 +0300

    Initial Commit

---
+++ MasterMind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasterMind
+//
+//  Created by rony klein on 8/5/15.
+//  Copyright (c) 2015 rony klein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
